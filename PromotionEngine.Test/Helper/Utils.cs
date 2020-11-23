d8f90281489312ce7c1ef295bdd765487dd04be0
Vinoth Thangaraj <vinotht.thangaraj@gmail.com>
2020-11-23 16:37:39 +0530

    UNIT TEST Added

---
+++ PromotionEngine.Test/Helper/Utils.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PromotionEngine.Contracts.Models;
+using PromotionEngine.Service.Repository;
+using PromotionEngine.Service;
+
+namespace PromotionEngine.UnitTest.Helper
+{
+    public static class Utils
+    {
+        public static ILogger<CheckoutProcess> GetLogger()
+        {
+            var logger = new Mock<ILogger<CheckoutProcess>>();
+            return logger.Object;
+        }
+
+        public static List<PromotionType> GetPromotionTypes()
+        {
+            var promotionTypes = new List<PromotionType>
+            {
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'A', NoOfUnits = 3}},
+                    Price = 130
+                },
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'B', NoOfUnits = 2}},
+                    Price = 45
+                },
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'C', NoOfUnits = 1},
+                    new CartDetail {SKUId = 'D', NoOfUnits = 1}},
+                    Price = 30
+                }
+            };
+            return promotionTypes;
+        }
+
+        public static List<PromotionType> GetPromotionTypeWithPercentage()
+        {
+            var promotionTypes = new List<PromotionType>
+            {
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'A', NoOfUnits = 1}},
+                    Percentage = 40,
+                },
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'B', NoOfUnits = 2}},
+                    Price = 45
+                },
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'C', NoOfUnits = 1},
+                    new CartDetail {SKUId = 'D', NoOfUnits = 1}},
+                    Price = 30
+                }
+            };
+            return promotionTypes;
+        }
+
+        public static List<PromotionType> GetPromotionTypeWithPercentageForSameItem()
+        {
+            var promotionTypes = new List<PromotionType>
+            {
+                 new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'A', NoOfUnits = 2}},
+                    Price = 30
+                },
+                new PromotionType
+                {
+                    CartDetails = new List<CartDetail> {
+                    new CartDetail {SKUId = 'A', NoOfUnits = 1}},
+                    Percentage = 40,
+                }
+            };
+            return promotionTypes;
+        }
+
+    }
+}
